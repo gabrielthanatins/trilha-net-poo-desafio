d72864fa1e18cab4f8bfde430965a43994140ab8
Gabriel Evaristo <gabriel-evaristo@hotmail.com>
2024-09-09 15:26:06 -0300

    Finalizando testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Teste com Nokia");
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789IMEI", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+System.Console.WriteLine("----------------");
+
+
+System.Console.WriteLine("Teste com iPhone");
+Smartphone iphone = new Iphone("123456789", "Iphone 13", "987654321IMEI", 250);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
